7c7af444f4e09375e27dbde2e09c1b6954a86df8
Hussain <hedayati1488@gmail.com>
2025-02-15 07:38:52 +0330

    First Commit

---
+++ src/main/java/CheckenProduction/CheckenProductionApplication.java
@@ -0,0 +1,13 @@
+package CheckenProduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckenProductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckenProductionApplication.class, args);
+	}
+
+}
